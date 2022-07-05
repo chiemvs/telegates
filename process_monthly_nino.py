5de0afb189d4baaf2468ba7b78f1114a17d1dd37
chiemvs <chiemvs@gmail.com>
2022-07-05 17:29:13 +0200

    Nino based compositing. Small script for processing monthly relative nino index of Oldenborgh (2021) which has a longer record and no warming trend.

---
+++ process_monthly_nino.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import netCDF4 as nc
+import xarray as xr
+
+monthly_inds = []
+for region in [34, 4]:
+    ind = xr.open_dataarray(f'/scistor/ivm/jsn295/paper4/nino{region}_rel_monthly.nc', decode_times = False) # Mid stamped
+    ind_series = ind.to_dataframe() # Lets do left stamping
+    ind_series.index = pd.date_range(ind.time.attrs['units'][-10:-3], periods = len(ind), freq = 'MS') # Left stamping with first day of the month
+    ind_series.columns = pd.MultiIndex.from_tuples([('nino',31,region,'mean')], names = ['variable','timeagg','clustid','metric'])
+    monthly_inds.append(ind_series)
+
+comb = pd.concat(monthly_inds, axis = 1).dropna()
+comb.to_hdf('/scistor/ivm/jsn295/paper4/nino_rel_monthly.h5', key = 'nino')
