32064bd7b153230ccdcd04f0bb2c567090a72f68
chiemvs <chiemvs@gmail.com>
2022-09-01 10:37:17 +0200

    Changes to matrix compositing code in notebook. Option to base anomalies on the last decades only cannot be in the notebook due to memory issues. Therefore incorporated as a separate precomputed option in the deseason scripts. Option to time aggregate before compositing has not been tested on the largest datasets.

--- hpc/deseason.py
+++ hpc/deseason.py
@@ -43,7 +43,7 @@ def evaluate_poly(array : xr.DataArray, coefs: xr.DataArray, year: int = None):
         y += X**degree * coefs.sel(coefs = degree, drop = True)
     return y
 
-def deseasonalize(array: xr.DataArray, per_year: bool = False, return_polyval: bool = False, degree = 3):
+def deseasonalize(array: xr.DataArray, per_year: bool = False, return_polyval: bool = False, degree = 3, normalslice: slice = slice(None)):
     """
     If per year then trend is removed (and likely also interannual variability)
     plus you'll get a jump on the first of january. (not so important for summer)
@@ -60,17 +60,19 @@ def deseasonalize(array: xr.DataArray, per_year: bool = False, return_polyval: b
             if return_polyval:
                 polyval.loc[polyval.time.dt.year == year,...] = yearly_polyval
     else:
-        polyval = evaluate_poly(array, coefs = fit_poly(array, degree = degree))
+        polyval = evaluate_poly(array, coefs = fit_poly(array.sel(time = normalslice), degree = degree))
         deseasonalized -= polyval
     if return_polyval:
         return deseasonalized, polyval
     else:
         return deseasonalized
 
-datadir = Path(os.path.expanduser('~/ERA5/'))
-anomdir = Path(os.path.expanduser('~/paper4/anomalies'))
 
-def make_anoms(name: str, blocksize = 50, degree: int = 3):
+def make_anoms(name: str, blocksize = 50, degree: int = 3, normalslice: slice = slice(None)):
+    """
+    Normalslice can be used to determine the period that is the normal
+    if not supplied the polynomials are computed on all available timesteps
+    """
     varname = name.split('_')[0]
     try:
         da = xr.open_dataarray(datadir / f'{name}.nc')
@@ -99,11 +101,15 @@ def make_anoms(name: str, blocksize = 50, degree: int = 3):
     
         temparr = da.isel(latitude = latslice, longitude = lonslice)
         
-        deseasonalized = deseasonalize(temparr) 
+        deseasonalized = deseasonalize(temparr, normalslice = normalslice) 
     
         wr.write_spatial_block(array = deseasonalized, latslice = latslice, lonslice = lonslice, units = da.units )
 
-#for name, deg in zip(['v300_nhnorm','u300_nhnorm'],[4,4]):
-#    make_anoms(name = name, degree = deg, blocksize = 100)
-#make_anoms(name = 't2m_europe', degree = 5, blocksize = 150)
-make_anoms(name = 'stream_nhnorm', degree = 4, blocksize = 150)
+datadir = Path(os.path.expanduser('~/ERA5/'))
+anomdir = Path(os.path.expanduser('~/paper4/anomalies20'))
+normalslice = slice('2000-01-01','2021-01-01')
+
+for name, deg in zip(['v300_nhnorm','u300_nhnorm','z300_nhnorm','sst_nhplus','olr_tropics'],[4,4,4,4,4]):
+    make_anoms(name = name, degree = deg, blocksize = 100, normalslice = normalslice)
+#make_anoms(name = 't2m_europe', degree = 5, blocksize = 150, normalslice = normalslice)
+make_anoms(name = 'stream_nhnorm', degree = 4, blocksize = 150, normalslice = normalslice)
